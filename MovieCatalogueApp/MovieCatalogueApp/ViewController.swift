466b801aa8e30a176a5d664355f4421f9bb3c8a3
vrushali2018 <130543195+vrushali2018@users.noreply.github.com>
2023-04-12 14:26:50 +0530

    Add files via upload
    initial commit

---
+++ MovieCatalogueApp/MovieCatalogueApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieCatalogueApp
+//
+//  Created by NEOSOFT1 on 12/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
